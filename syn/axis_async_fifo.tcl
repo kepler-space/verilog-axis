4517792f56142da366939a93e8b6a91b6ab98ec5
William Kingsford <wkingsford@kepler.space>
2022-04-15 15:49:44 +1200

    IMPL: tcl. axis_async_fifo.tcl now raises critical warnings, not errors
    Previously, if there was an error while setting constraints for any FIFO
    instance, the script would encounter an error and terminate, leading to
    constraints not being applied for any subsequent FIFOs. Now, errors are
    caught using a Tcl `catch` statement, and re-raised as critical
    warnings, which do not cause the whole script to terminate.

--- syn/axis_async_fifo.tcl
+++ syn/axis_async_fifo.tcl
@@ -1,15 +1,15 @@
 # Copyright (c) 2019 Alex Forencich
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,82 +21,88 @@
 # AXI stream asynchronous FIFO timing constraints
 
 foreach fifo_inst [get_cells -quiet -hier -filter {(ORIG_REF_NAME == axis_async_fifo || REF_NAME == axis_async_fifo)}] {
-    puts "Inserting timing constraints for axis_async_fifo instance $fifo_inst"
+    # Set the constraints from within a catch block, so that if any of the statements fail to match any objects (e.g. because
+    # one side of the FIFO is disconnected and gets optimized out), the script still completes.
+    if { [catch {
+        puts "Inserting timing constraints for axis_async_fifo instance $fifo_inst"
 
-    # get clock periods
-    set read_clk [get_clocks -of_objects [get_pins $fifo_inst/rd_ptr_reg_reg[0]/C]]
-    set write_clk [get_clocks -of_objects [get_pins $fifo_inst/wr_ptr_reg_reg[0]/C]]
+        # get clock periods
+        set read_clk [get_clocks -of_objects [get_pins $fifo_inst/rd_ptr_reg_reg[0]/C]]
+        set write_clk [get_clocks -of_objects [get_pins $fifo_inst/wr_ptr_reg_reg[0]/C]]
 
-    set read_clk_period [get_property -min PERIOD $read_clk]
-    set write_clk_period [get_property -min PERIOD $write_clk]
+        set read_clk_period [get_property -min PERIOD $read_clk]
+        set write_clk_period [get_property -min PERIOD $write_clk]
 
-    set min_clk_period [expr $read_clk_period < $write_clk_period ? $read_clk_period : $write_clk_period]
+        set min_clk_period [expr $read_clk_period < $write_clk_period ? $read_clk_period : $write_clk_period]
 
-    # reset synchronization
-    set reset_ffs [get_cells -quiet -hier -regexp ".*/(s|m)_rst_sync\[123\]_reg_reg" -filter "PARENT == $fifo_inst"]
+        # reset synchronization
+        set reset_ffs [get_cells -quiet -hier -regexp ".*/(s|m)_rst_sync\[123\]_reg_reg" -filter "PARENT == $fifo_inst"]
 
-    if {[llength $reset_ffs]} {
-        set_property ASYNC_REG TRUE $reset_ffs
-        set_false_path -to [get_pins -of_objects $reset_ffs -filter {IS_PRESET || IS_RESET}]
-    }
-
-    if {[llength [get_cells -quiet $fifo_inst/s_rst_sync2_reg_reg]]} {
-        set_false_path -to [get_pins $fifo_inst/s_rst_sync2_reg_reg/D]
-        set_max_delay  -from [get_cells $fifo_inst/s_rst_sync2_reg_reg] -to [get_cells $fifo_inst/s_rst_sync3_reg_reg] $min_clk_period
-    }
-
-    if {[llength [get_cells -quiet $fifo_inst/m_rst_sync2_reg_reg]]} {
-        set_false_path -to [get_pins $fifo_inst/m_rst_sync2_reg_reg/D]
-        set_max_delay  -from [get_cells $fifo_inst/m_rst_sync2_reg_reg] -to [get_cells $fifo_inst/m_rst_sync3_reg_reg] $min_clk_period
-    }
-
-    # pointer synchronization
-    set_property ASYNC_REG TRUE [get_cells -hier -regexp ".*/(wr|rd)_ptr_gray_sync\[12\]_reg_reg\\\[\\d+\\\]" -filter "PARENT == $fifo_inst"]
-
-    set_max_delay -from [get_cells "$fifo_inst/rd_ptr_reg_reg[*] $fifo_inst/rd_ptr_gray_reg_reg[*]"] -to [get_cells $fifo_inst/rd_ptr_gray_sync1_reg_reg[*]] -datapath_only $read_clk_period
-    set_bus_skew  -from [get_cells "$fifo_inst/rd_ptr_reg_reg[*] $fifo_inst/rd_ptr_gray_reg_reg[*]"] -to [get_cells $fifo_inst/rd_ptr_gray_sync1_reg_reg[*]] $write_clk_period
-    set_max_delay -from [get_cells -quiet "$fifo_inst/wr_ptr_reg_reg[*] $fifo_inst/wr_ptr_gray_reg_reg[*] $fifo_inst/wr_ptr_sync_gray_reg_reg[*]"] -to [get_cells $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]] -datapath_only $write_clk_period
-    set_bus_skew  -from [get_cells -quiet "$fifo_inst/wr_ptr_reg_reg[*] $fifo_inst/wr_ptr_gray_reg_reg[*] $fifo_inst/wr_ptr_sync_gray_reg_reg[*]"] -to [get_cells $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]] $read_clk_period
-
-    # output register (needed for distributed RAM sync write/async read)
-    set output_reg_ffs [get_cells -quiet "$fifo_inst/m_axis_pipe_reg_reg[0][*]"]
-
-    if {[llength $output_reg_ffs]} {
-        set_false_path -from $write_clk -to $output_reg_ffs
-    }
-
-    # frame FIFO pointer update synchronization
-    set update_ffs [get_cells -quiet -hier -regexp ".*/wr_ptr_update(_ack)?_sync\[123\]_reg_reg" -filter "PARENT == $fifo_inst"]
-
-    if {[llength $update_ffs]} {
-        set_property ASYNC_REG TRUE $update_ffs
-
-        set_max_delay -from [get_cells $fifo_inst/wr_ptr_update_reg_reg] -to [get_cells $fifo_inst/wr_ptr_update_sync1_reg_reg] -datapath_only $write_clk_period
-        set_max_delay -from [get_cells $fifo_inst/wr_ptr_update_sync3_reg_reg] -to [get_cells $fifo_inst/wr_ptr_update_ack_sync1_reg_reg] -datapath_only $read_clk_period
-    }
-
-    # status synchronization
-    foreach i {overflow bad_frame good_frame} {
-        set status_sync_regs [get_cells -quiet -hier -regexp ".*/${i}_sync\[123\]_reg_reg" -filter "PARENT == $fifo_inst"]
-
-        if {[llength $status_sync_regs]} {
-            set_property ASYNC_REG TRUE $status_sync_regs
-
-            set_max_delay -from [get_cells $fifo_inst/${i}_sync1_reg_reg] -to [get_cells $fifo_inst/${i}_sync2_reg_reg] -datapath_only $read_clk_period
+        if {[llength $reset_ffs]} {
+            set_property ASYNC_REG TRUE $reset_ffs
+            set_false_path -to [get_pins -of_objects $reset_ffs -filter {IS_PRESET || IS_RESET}]
         }
+
+        if {[llength [get_cells -quiet $fifo_inst/s_rst_sync2_reg_reg]]} {
+            set_false_path -to [get_pins $fifo_inst/s_rst_sync2_reg_reg/D]
+            set_max_delay  -from [get_cells $fifo_inst/s_rst_sync2_reg_reg] -to [get_cells $fifo_inst/s_rst_sync3_reg_reg] $min_clk_period
+        }
+
+        if {[llength [get_cells -quiet $fifo_inst/m_rst_sync2_reg_reg]]} {
+            set_false_path -to [get_pins $fifo_inst/m_rst_sync2_reg_reg/D]
+            set_max_delay  -from [get_cells $fifo_inst/m_rst_sync2_reg_reg] -to [get_cells $fifo_inst/m_rst_sync3_reg_reg] $min_clk_period
+        }
+
+        # pointer synchronization
+        set_property ASYNC_REG TRUE [get_cells -hier -regexp ".*/(wr|rd)_ptr_gray_sync\[12\]_reg_reg\\\[\\d+\\\]" -filter "PARENT == $fifo_inst"]
+
+        set_max_delay -from [get_cells "$fifo_inst/rd_ptr_reg_reg[*] $fifo_inst/rd_ptr_gray_reg_reg[*]"] -to [get_cells $fifo_inst/rd_ptr_gray_sync1_reg_reg[*]] -datapath_only $read_clk_period
+        set_bus_skew  -from [get_cells "$fifo_inst/rd_ptr_reg_reg[*] $fifo_inst/rd_ptr_gray_reg_reg[*]"] -to [get_cells $fifo_inst/rd_ptr_gray_sync1_reg_reg[*]] $write_clk_period
+        set_max_delay -from [get_cells -quiet "$fifo_inst/wr_ptr_reg_reg[*] $fifo_inst/wr_ptr_gray_reg_reg[*] $fifo_inst/wr_ptr_sync_gray_reg_reg[*]"] -to [get_cells $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]] -datapath_only $write_clk_period
+        set_bus_skew  -from [get_cells -quiet "$fifo_inst/wr_ptr_reg_reg[*] $fifo_inst/wr_ptr_gray_reg_reg[*] $fifo_inst/wr_ptr_sync_gray_reg_reg[*]"] -to [get_cells $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]] $read_clk_period
+
+        # output register (needed for distributed RAM sync write/async read)
+        set output_reg_ffs [get_cells -quiet "$fifo_inst/m_axis_pipe_reg_reg[0][*]"]
+
+        if {[llength $output_reg_ffs]} {
+            set_false_path -from $write_clk -to $output_reg_ffs
+        }
+
+        # frame FIFO pointer update synchronization
+        set update_ffs [get_cells -quiet -hier -regexp ".*/wr_ptr_update(_ack)?_sync\[123\]_reg_reg" -filter "PARENT == $fifo_inst"]
+
+        if {[llength $update_ffs]} {
+            set_property ASYNC_REG TRUE $update_ffs
+
+            set_max_delay -from [get_cells $fifo_inst/wr_ptr_update_reg_reg] -to [get_cells $fifo_inst/wr_ptr_update_sync1_reg_reg] -datapath_only $write_clk_period
+            set_max_delay -from [get_cells $fifo_inst/wr_ptr_update_sync3_reg_reg] -to [get_cells $fifo_inst/wr_ptr_update_ack_sync1_reg_reg] -datapath_only $read_clk_period
+        }
+
+        # status synchronization
+        foreach i {overflow bad_frame good_frame} {
+            set status_sync_regs [get_cells -quiet -hier -regexp ".*/${i}_sync\[123\]_reg_reg" -filter "PARENT == $fifo_inst"]
+
+            if {[llength $status_sync_regs]} {
+                set_property ASYNC_REG TRUE $status_sync_regs
+
+                set_max_delay -from [get_cells $fifo_inst/${i}_sync1_reg_reg] -to [get_cells $fifo_inst/${i}_sync2_reg_reg] -datapath_only $read_clk_period
+            }
+        }
+
+        # CDC-6 warning is not applicable since this bus is Gray coded
+        create_waiver -type CDC -id {CDC-6} -user "axis_async_fifo"\
+        -desc "The CDC-6 warning is waived for the read pointer in axis_async_fifo, since it is Gray coded." \
+        -from [get_pins "$fifo_inst/rd_ptr_reg_reg[*]/* $fifo_inst/rd_ptr_gray_reg_reg[*]/*"] -to [get_pins $fifo_inst/rd_ptr_gray_sync1_reg_reg[*]/*]
+        create_waiver -type CDC -id {CDC-6} -user "axis_async_fifo"\
+        -desc "The CDC-6 warning is waived for the write pointer in axis_async_fifo, since it is Gray coded." \
+        -from [get_pins -quiet "$fifo_inst/wr_ptr_reg_reg[*]/* $fifo_inst/wr_ptr_gray_reg_reg[*]/* $fifo_inst/wr_ptr_sync_gray_reg_reg[*]/*"] -to [get_pins $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]/*]
+
+        # When used as a frame FIFO, Vivado incorrectly identifies the write pointer gating as being
+        # a clock-enable CDC structure
+        create_waiver -type CDC -id {CDC-15} -user "axis_async_fifo"\
+        -desc "The CDC-15 warning is waived for the write pointer in axis_async_fifo, since it is Gray coded." \
+        -from [get_pins -quiet "$fifo_inst/wr_ptr_reg_reg[*]/* $fifo_inst/wr_ptr_gray_reg_reg[*]/* $fifo_inst/wr_ptr_sync_gray_reg_reg[*]/*"] -to [get_pins $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]/*]
+    } errorMessage] } {
+        send_msg_id {AXIS 1-1} {CRITICAL WARNING} "axis_async_fifo instance $fifo_inst raised the following error:\n$errorMessage"
     }
-
-    # CDC-6 warning is not applicable since this bus is Gray coded
-    create_waiver -type CDC -id {CDC-6} -user "axis_async_fifo"\
-    -desc "The CDC-6 warning is waived for the read pointer in axis_async_fifo, since it is Gray coded." \
-    -from [get_pins "$fifo_inst/rd_ptr_reg_reg[*]/* $fifo_inst/rd_ptr_gray_reg_reg[*]/*"] -to [get_pins $fifo_inst/rd_ptr_gray_sync1_reg_reg[*]/*]
-    create_waiver -type CDC -id {CDC-6} -user "axis_async_fifo"\
-    -desc "The CDC-6 warning is waived for the write pointer in axis_async_fifo, since it is Gray coded." \
-    -from [get_pins -quiet "$fifo_inst/wr_ptr_reg_reg[*]/* $fifo_inst/wr_ptr_gray_reg_reg[*]/* $fifo_inst/wr_ptr_sync_gray_reg_reg[*]/*"] -to [get_pins $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]/*]
-
-    # When used as a frame FIFO, Vivado incorrectly identifies the write pointer gating as being
-    # a clock-enable CDC structure
-    create_waiver -type CDC -id {CDC-15} -user "axis_async_fifo"\
-    -desc "The CDC-15 warning is waived for the write pointer in axis_async_fifo, since it is Gray coded." \
-    -from [get_pins -quiet "$fifo_inst/wr_ptr_reg_reg[*]/* $fifo_inst/wr_ptr_gray_reg_reg[*]/* $fifo_inst/wr_ptr_sync_gray_reg_reg[*]/*"] -to [get_pins $fifo_inst/wr_ptr_gray_sync1_reg_reg[*]/*]
 }
